cce0390882d593480ea40405db50a9b88f384a8e
fx-145 <fxlecoeur2@gmail.com>
2024-07-09 15:03:21 +0200

    crud habitats

--- app/models/HomeModel.php
+++ app/models/HomeModel.php
@@ -35,10 +35,10 @@ class HomeModel
 
     // Requête  pour récupérer les habitats et une seule photo pour illustrer la page Homes, 
 //triée et ensuite filtrée par home_picture_id le plus ancien
-public function getHomesAndOnePicture()
-{
-    try {
-        $query = "SELECT h.*, p.*
+    public function getHomesAndOnePicture()
+    {
+        try {
+            $query = "SELECT h.*, p.*
         FROM homes h
         INNER JOIN (
             SELECT home_id, MIN(home_picture_id) AS min_home_picture_id
@@ -48,57 +48,145 @@ public function getHomesAndOnePicture()
         ON h.home_id = sub.home_id
         INNER JOIN home_pictures p
         ON sub.home_id = p.home_id AND sub.min_home_picture_id = p.home_picture_id";
-        $statement = $this->db->prepare($query);
-       // $statement->bindParam(':email', $emailForm, PDO::PARAM_STR);
-        $statement->execute();
-       //c'est ça qui faisait planter le code:  $statement->fetchAll(PDO::FETCH_ASSOC);
-        if ($statement) {
-         //   var_dump($statement);
-            return $statement;
-            
-
-        } else {
-            echo "Impossible de récupérer le role_name";
-            //return null;
-        }
-    } catch (PDOException $e) {
-        echo "Erreur : " . $e->getMessage();
-        return null;
-    }
-
-}
-
-// (feuille homeDetails.php)Recenser tous les infos d'un habitat: la description, mais aussi les animaux rattachés
-public function getHomeAndAnimalsDetails($home_id)
-{
-    
-    {
-         try {
-            $query = "SELECT homes.*, animals.* FROM homes
-            INNER JOIN animals ON homes.home_id = animals.home_id
-            WHERE homes.home_id = :home_id";
-            //$query = "SELECT home_name FROM homes WHERE home_id = ?";
             $statement = $this->db->prepare($query);
-            $statement->bindParam(':home_id', $home_id, PDO::PARAM_INT);
+            // $statement->bindParam(':email', $emailForm, PDO::PARAM_STR);
             $statement->execute();
-            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-            
-            //var_dump($result)['home_description'];
-            if ($result) {
-                return $result;
+            //c'est ça qui faisait planter le code:  $statement->fetchAll(PDO::FETCH_ASSOC);
+            if ($statement) {
+                //   var_dump($statement);
+                return $statement;
+
+
             } else {
-                echo "pas d'enregistrement";
+                echo "Impossible de récupérer le role_name";
                 //return null;
             }
-            
         } catch (PDOException $e) {
             echo "Erreur : " . $e->getMessage();
             return null;
-
-
         }
 
     }
-}
 
+    // (feuille homeDetails.php)Recenser tous les infos d'un habitat: la description, mais aussi les animaux rattachés
+    public function getHomeAndAnimalsDetails($home_id)
+    { {
+            try {
+                $query = "SELECT homes.*, animals.* FROM homes
+            INNER JOIN animals ON homes.home_id = animals.home_id
+            WHERE homes.home_id = :home_id";
+                //$query = "SELECT home_name FROM homes WHERE home_id = ?";
+                $statement = $this->db->prepare($query);
+                $statement->bindParam(':home_id', $home_id, PDO::PARAM_INT);
+                $statement->execute();
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+                //var_dump($result)['home_description'];
+                if ($result) {
+                    return $result;
+                } else {
+                    echo "pas d'enregistrement";
+                    //return null;
+                }
+
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
+                return null;
+
+
+            }
+
+        }
+    }
+    public function getHomeWithId($home_id)
+    {
+        // Recenser tous les habitats
+        {
+            try {
+                $query = "SELECT * FROM homes WHERE home_id = :home_id";
+                //$query = "SELECT home_name FROM homes WHERE home_id = ?";
+                $statement = $this->db->prepare($query);
+                $statement->bindParam(':home_id', $home_id, PDO::PARAM_INT);
+                $statement->execute();
+                $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+                //var_dump($result)['home_name'];
+                if ($result) {
+                    return $result;
+                } else {
+                    echo "pas d'enregistrement";
+                    //return null;
+                }
+
+            } catch (PDOException $e) {
+                echo "Erreur : " . $e->getMessage();
+                return null;
+
+
+            }
+
+        }
+    }
+    public function updateHome($home_id, $newHomeName, $newHomeDescription)
+    {
+
+        try {
+
+            // Préparer une requête pour la mise à jour des données dans la table "homes"
+            $insertQuery = "UPDATE homes SET home_name= :home_name, home_description=:home_description WHERE home_id=:home_id";
+            $stmt = $this->db->prepare($insertQuery);
+            //$stmt->execute(array(':home_name' => $home_name, ':home_description' => $home_description, ':home_id' => $home_id));
+            $stmt->bindParam(':home_name', $newHomeName, PDO::PARAM_STR);
+            $stmt->bindParam(':home_description', $newHomeDescription, PDO::PARAM_STR);
+            $stmt->bindParam(':home_id', $home_id, PDO::PARAM_INT);
+            var_dump($newHomeName);
+            $stmt->execute();
+            
+        } catch (PDOException $e) {
+            echo "Erreur lors de l'enregistrement des données : " . $e->getMessage();
+        }
+    }
+
+        // ajouter un nouvel habitat
+    public function addHome($home_name, $home_description)
+    {
+        try {
+
+            // Préparer une requête pour la création de données dans la table "homes"
+            $insertQuery = "INSERT INTO homes (home_name, home_description)
+            VALUES (:home_name, :home_description)";
+            $stmt = $this->db->prepare($insertQuery);
+            $stmt->execute(array(':home_name' => $home_name, ':home_description' => $home_description));
+           
+           if ($stmt) {
+            echo "Habitat ajouté avec succès";
+
+       
+        }
+
+            echo "Nouvel habitat enregistré";
+        } catch (PDOException $e) {
+            echo "Erreur lors de l'enregistrement des données : " . $e->getMessage();
+        }
+    }
+
+    //supprimer un habitat et ses données
+    public function deleteHome($home_id)
+    {
+        try {
+
+            // Préparer une requête pour la suppression des données dans la table "homes"
+            $deleteQuery = "DELETE FROM homes WHERE home_id=?";
+            $stmt = $this->db->prepare($deleteQuery);
+            $stmt->execute([$home_id]);
+            if ($stmt) {
+                echo "Habitat supprimé avec succès";
+
+            }
+
+
+        } catch (PDOException $e) {
+            echo "Erreur lors de l'enregistrement des données : " . $e->getMessage();
+        }
+    }
 }
---
+++ app/views/crud/crud_homes_update.php
@@ -0,0 +1,91 @@
+<?php
+//include '../Model/HomeModel.php';
+require_once 'app/controllers/HomeController.php';
+require_once 'app/controllers/PictureController.php';
+ 
+// Vérification si le formulaire a été soumis
+if (isset($_POST['home_id'])) {
+  // Récupération des données du formulaire
+  $home_id = $_POST['home_id'];
+
+  // Création d'une instance du modèle HomeModel
+  $controller = new HomeController();
+  $result = $controller->getHomeWithId($home_id);
+  $home_name = $result['home_name'];
+  $home_description = $result['home_description'];
+
+  // Création d'une instance du modèle PictureModel pour afficher les photos de l'habitat
+  $controllerPics = new PictureController();
+  $resultPics = $controllerPics->getHomePictures($home_id);
+}
+?>
+
+    <!-- Appel de l'area admin -->
+    <?php include_once("app/views/elements/admin_area.php"); ?>
+    <!-- Affichage de la sidebar -->
+    <?php include_once("app/views/layouts/sidebar_admin.php"); ?>
+    <!-- Affichage de la navbar -->
+    <div id="content">
+        <?php include_once("app/views/layouts/navbar.php"); ?>
+        <br>
+        <body>
+        <button class="btn btn-success mx-2" id="menu-toggle"><></button>
+        <div class="container mt-4">
+            <h1 class="mb-4">Modifier un habitat</h1>
+            <form action="crudHomeHandler" method="post">
+                <input type="hidden" value="<?php echo $home_id; ?>" id="home_id" name="home_id">
+                <div class="form-group">
+                    <label for="home_name">Nom de l'habitat à modifier:</label>
+                    <input value="<?php echo htmlspecialchars($home_name, ENT_QUOTES, 'UTF-8'); ?>" type="text" class="form-control" id="home_name" name="newhome_name" required>
+                </div>
+                <div class="form-group">
+                    <label for="home_description">Description:</label>
+                    <textarea class="form-control" id="home_description" name="newhome_description" rows="4" required><?php echo htmlspecialchars($home_description, ENT_QUOTES, 'UTF-8'); ?></textarea>
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary" name="submit_update_home">Modifier</button>
+                <a href="crud_homes.php" class="btn btn-secondary ml-2">Annuler</a>
+            </form>
+            <hr>
+            <!-- Tableau Bootstrap affichage de toutes les photos des habitats -->
+            <h2 class="mt-4">Photos de l'habitat</h2>
+            <form action="../Controller/PictureController.php" method="post">
+                <table class="table table-striped table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Photos</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($resultPics as $row): ?>
+                            <tr>
+                                <td><img src="<?php echo "../../" . htmlspecialchars($row['home_picture_path'], ENT_QUOTES, 'UTF-8'); ?>" alt="Photo de l'habitat" class="img-fluid" width="200"></td>
+                                <td>
+                                    <input type="hidden" name="home_id" value="<?php echo htmlspecialchars($home_id, ENT_QUOTES, 'UTF-8'); ?>">
+                                    <input type="hidden" name="home_name" value="<?php echo htmlspecialchars($home_name, ENT_QUOTES, 'UTF-8'); ?>">
+                                    <input type="hidden" name="home_picture_id" value="<?php echo htmlspecialchars($row['home_picture_id'], ENT_QUOTES, 'UTF-8'); ?>">
+                                    <input type="hidden" name="home_picture_path" value="<?php echo htmlspecialchars($row['home_picture_path'], ENT_QUOTES, 'UTF-8'); ?>">
+                                    <button type="submit" name="submit_delete_home_picture" class="btn btn-danger">Supprimer</button>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </form>
+            <form action="crud_homes_pictures_add.php" method="post" class="mt-2">
+                <input type="hidden" name="home_id" value="<?php echo htmlspecialchars($home_id, ENT_QUOTES, 'UTF-8'); ?>">
+                <input type="hidden" name="home_name" value="<?php echo htmlspecialchars($home_name, ENT_QUOTES, 'UTF-8'); ?>">
+                <button type="submit" class="btn btn-success">Ajouter Photo</button>
+            </form>
+        </div>
+        <br>
+        <!-- Appel du footer -->
+        <?php include("app/views/layouts/footer.php"); ?>
+        <!-- Appel des scripts -->
+        <?php include("app/views/layouts/scripts.php"); ?>
+        <!-- Appel du script de la sidebar --> 
+        <script src="public/js/sidebar_script.js"></script>
+    </div>
+</body>
+</html>

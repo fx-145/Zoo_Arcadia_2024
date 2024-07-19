80ad691f7bc56f679adc4d35ea93c331890ba885
fx-145 <fxlecoeur@gmail.com>
2024-07-19 14:31:44 +0200

    test

--- test.php
+++ test.php
@@ -8,12 +8,13 @@ try {
     require_once __DIR__ . '/app/controllers/router/Router.controller.php';
     if($_SERVER['REQUEST_URI'] == '/index.php') {
         $_SERVER['REQUEST_URI'] = '/';
+        $controller = new Navbar();
+        $uri = $controller->getCurrentUri();
+        $result = $controller->router($uri);
+        var_dump($result);
     }
-    $controller = new Navbar();
-    $uri = $controller->getCurrentUri();
-    $result = $controller->router($uri);
-    var_dump($result);
+    
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
-    return null;
+    //return null;
 }

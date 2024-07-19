4e1d2f2b54f1bd32d87916fa6ec664ecf22ec5e3
fx-145 <fxlecoeur@gmail.com>
2024-07-19 13:25:54 +0200

    test

--- test.php
+++ test.php
@@ -6,7 +6,13 @@ var_dump(getenv('DB_HOST'));
 
 try {
     require_once __DIR__ . '/app/controllers/router/Router.controller.php';
-    echo 'ok';    
+    if($_SERVER['REQUEST_URI'] == '/index.php') {
+        $_SERVER['REQUEST_URI'] = '/';
+    }
+    $controller = new Navbar();
+    $uri = $controller->getCurrentUri();
+    $result = $controller->router($uri);
+    var_dump($result);
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
     return null;

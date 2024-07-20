e2e362fd455f43db7cefbe2ff3cf73a225d72ab8
fx-145 <fxlecoeur@gmail.com>
2024-07-20 23:31:45 +0200

    test

--- app/controllers/MailController.php
+++ app/controllers/MailController.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '../../../config.php';
 require_once APP_MODEL_PATH . '/MailModel.php';
-require_once __DIR__ . '../handler/MailHandler.php';
+//require_once __DIR__ . '../handler/MailHandler.php';
 
 class MailController
 {

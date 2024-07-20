2edcdf8ac5c10442ad9553aa34c0d73b53badfcd
fx-145 <fxlecoeur@gmail.com>
2024-07-20 23:13:47 +0200

    test

---
+++ app/controllers/handler/MailHandler.php
@@ -0,0 +1,5 @@
+<?php
+//require_once __DIR__ . '/../MailController.php';
+//require_once __DIR__ . '/../router/Router.controller.php';
+require_once __DIR__ . '/app/controllers/MailController.php';
+require_once __DIR__ . '/app/controllers/router/Router.controller.php';

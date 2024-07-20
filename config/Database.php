fa69307f9fcd4cfbb03b743c0eeeb5464c8870b1
fx-145 <fxlecoeur@gmail.com>
2024-07-20 09:43:56 +0200

    test

--- config/Database.php
+++ config/Database.php
@@ -33,5 +33,33 @@ class Database {
         }
     }
 }
+class AnimalModel
+{
+private $db;
+    //appel de la base de donnée stockée dans le fichier Database, et dans la classe Database.
+    public function __construct()
+    {
+        $database = new Database();
+        $this->db = $database->db;
+    }
+    public function getAnimals()
+    {
+        try {
+            $query = "SELECT DISTINCT race FROM animals ORDER BY home_id";
+            $statement = $this->db->prepare($query);
+            $statement->execute();
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
 
+            if ($statement) {
+                return $statement;
+            } else {
+                echo "Pas d'enregistrement";
+                return null;
+            }
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+            return null;
+        }
+    }
 
+}
--- test.php
+++ test.php
@@ -9,7 +9,59 @@ var_dump($controller);
 $uri = $controller->getCurrentUri();
 var_dump($uri);
 $result = $controller->router($uri);
-var_dump($result);
+//var_dump($result);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$envPath = __DIR__ . '/../';
+error_log("Loading .env file from: " . $envPath);
+
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+Class Database {
+
+    private $host;
+    private $db_name;
+    private $user_name;
+    private $password;
+    public $db;
+
+    public function __construct() {
+        $this->host = getenv('DB_HOST') ?: $_ENV['DB_HOST'];
+        $this->db_name = getenv('DB_NAME') ?: $_ENV['DB_NAME'];
+        $this->user_name = getenv('DB_USER') ?: $_ENV['DB_USER'];
+        $this->password = getenv('DB_PASS') ?: $_ENV['DB_PASS'];
+
+        try {
+            $this->db = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->user_name, $this->password);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+            die();
+        }
+    }
+}
+
+class AnimalController
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new AnimalModel();
+    }
+    public function getAnimals()
+    {
+        return $this->model->getAnimals();
+    }
+}
+
+//Pour lister les animaux: crée une instance d'AnimalController, et un appel de la méthode getAnimalRaces
+$controllerA = new AnimalController();
+$resultA = $controllerA->getAnimals();
+var_dump($resultA);
+
 
 
 

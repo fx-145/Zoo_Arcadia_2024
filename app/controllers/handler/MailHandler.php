3d94c16369802b4a3c087bf1899a6f0ffd9e116e
fx-145 <fxlecoeur@gmail.com>
2024-07-20 23:07:29 +0200

    test mail

--- app/controllers/handler/MailHandler.php
+++ app/controllers/handler/MailHandler.php
@@ -1,6 +1,12 @@
 <?php
-require_once __DIR__ . '/../MailController.php';
-require_once __DIR__ . '/../router/Router.controller.php';
+//require_once __DIR__ . '/../MailController.php';
+//require_once __DIR__ . '/../router/Router.controller.php';
+require_once __DIR__ . '/app/controllers/MailController.php';
+require_once __DIR__ . '/app/controllers/router/Router.controller.php';
+
+// Votre logique de code ici
+?>
+
 class MailHandler
 {
     private $controller;

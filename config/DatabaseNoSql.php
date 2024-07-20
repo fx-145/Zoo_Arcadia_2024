9b88309f6f30834fc65757a60da98bf512b4d5d8
fx-145 <fxlecoeur@gmail.com>
2024-07-20 21:24:54 +0200

    mongodb

--- config/DatabaseNoSql.php
+++ config/DatabaseNoSql.php
@@ -12,9 +12,11 @@ class DatabaseMongo {
 
     private function __construct() {
        // $this->client = new MongoDB\Client("mongodb://localhost:27017");
-        $this->client = new MongoDB\Client($_ENV['DB_HOST_M']);
+        //$this->client = new MongoDB\Client($_ENV['DB_HOST_M']);
+        $this->client = new MongoDB\Client(getenv('DB_HOST_M'));
        // $this->database = $this->client->selectDatabase('nom_de_la_base_de_donnees');
-        $this->database = $this->client->selectDatabase($_ENV['DB_NAME_M']);
+        //$this->database = $this->client->selectDatabase($_ENV['DB_NAME_M']);
+        $this->database = $this->client->selectDatabase(getenv('DB_NAME_M'));
     }
 
     public static function getInstance() {

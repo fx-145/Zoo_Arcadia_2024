6cd633ffb6c878bfb88b69ed739e2755bd0ad42c
fx-145 <fxlecoeur@gmail.com>
2024-07-20 22:54:45 +0200

    test mongo

--- config/DatabaseNoSql.php
+++ config/DatabaseNoSql.php
@@ -1,9 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-//appelle les variables déclarées dans .env
-//use Dotenv\Dotenv;
-//$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-//$dotenv->load();
 
 class DatabaseMongo {
     private static $instance = null;
@@ -11,12 +7,21 @@ class DatabaseMongo {
     private $database;
 
     private function __construct() {
-       // $this->client = new MongoDB\Client("mongodb://localhost:27017");
-        //$this->client = new MongoDB\Client($_ENV['DB_HOST_M']);
-        $this->client = new MongoDB\Client(getenv('DB_HOST_M'));
-       // $this->database = $this->client->selectDatabase('nom_de_la_base_de_donnees');
-        //$this->database = $this->client->selectDatabase($_ENV['DB_NAME_M']);
-        $this->database = $this->client->selectDatabase(getenv('DB_NAME_M'));
+        // Utiliser getenv pour récupérer l'URI de MongoDB
+        $uri = getenv('MONGODB_URI');
+        if ($uri === false) {
+            throw new Exception('La variable d\'environnement MONGODB_URI doit être définie.');
+        }
+
+        // Créer une connexion au client MongoDB
+        $this->client = new MongoDB\Client($uri);
+
+        // Sélectionner la base de données
+        $dbName = getenv('DB_NAME_M');
+        if ($dbName === false) {
+            throw new Exception('La variable d\'environnement DB_NAME_M doit être définie.');
+        }
+        $this->database = $this->client->selectDatabase($dbName);
     }
 
     public static function getInstance() {

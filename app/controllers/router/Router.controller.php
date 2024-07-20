e2d0404ec365bd04094a0e827d89d9585d55c70f
fx-145 <fxlecoeur@gmail.com>
2024-07-20 11:04:56 +0200

    back

--- app/controllers/router/Router.controller.php
+++ app/controllers/router/Router.controller.php
@@ -2,23 +2,16 @@
 
 class Navbar
 {
-    // Normaliser l'URI actuelle
-    public function normalizeUri($uri)
-    {
-        if ($uri == '/index.php') {
-            return '/';
-        }
-        return $uri;
-    }
-
-    // Retourne l'URI actuelle
+    // Retourne l'URl actuelle
     public function getCurrentUri()
     {
         return strtok($_SERVER['REQUEST_URI'], '?');
     }
 
+    
     public function urlValue($value, $params = []) {
         // Générer l'URL avec les paramètres fournis
+        // $params est facultatif, si un message est présent dans l'Url on va l'utiliser
         $url = $value;
         if (!empty($params)) {
             $queryString = http_build_query($params);
@@ -29,76 +22,68 @@ class Navbar
 
     public function router($uri)
     {
-        // Normaliser l'URI
-        $uri = $this->normalizeUri($uri);
         $uri = str_replace('/app', '', $uri);
-        var_dump($uri);
-        
-
         $routes = [
-            '/'  => __DIR__ . '/../../views/index.view.php',
-            '/services' => __DIR__ . '/../../views/services.view.php',
-            '/homes' => __DIR__ . '/../../views/homes.view.php',
-            '/connection' => __DIR__ . '/../../views/connection.view.php',
-            '/contact' => __DIR__ . '/../../views/contact.view.php',
-            '/admin' => __DIR__ . '/../../views/admin_area.view.php',
-            '/vet' => __DIR__ . '/../../views/vet_area.view.php',
-            '/employee' => __DIR__ . '/../../views/employee_area.view.php',
-            '/information'=> __DIR__ . '/../../views/information.php',
-            '/opinionStatusUpdate' => __DIR__ . '/../../controllers/handler/opinionStatusUpdate.php',
-            '/employee_validation_opinion' => __DIR__ . '/../../views/employee_validation_opinions.php',
-            '/opinionSend' => __DIR__ . '/../../controllers/handler/opinionSend.php',
-            '/admin_area'=> __DIR__ . '/../../views/admin_area.view.php',
-            '/employee_area'=> __DIR__ . '/../../views/employee_area.view.php',
-            '/vet_area'=> __DIR__ . '/../../views/vet_area.view.php',
-            '/homeDetails'=> __DIR__ . '/../../views/homeDetails.php',
-            '/homeDetailsHandler'=> __DIR__ . '/../../controllers/handler/homeDetailsHandler.php',
-            '/employee_food_form' => __DIR__ . '/../../views/employee_food_form.php',
-            '/employee_report_handler' => __DIR__ . '/../../controllers/handler/employee_report_handler.php',
-            '/vet_visit_form' => __DIR__ . '/../../views/vet_visit_form.php',
-            '/vet_reports_for_admin' => __DIR__ . '/../../views/vet_reports_for_admin.php',
-            '/end_session' => __DIR__ . '/../../controllers/handler/endSession.php',
-            '/crud_homes' => __DIR__ . '/../../views/crud/crud_homes.php',
-            '/crud_homes_update' => __DIR__ . '/../../views/crud/crud_homes_update.php',
-            '/crudHomeHandler'=> __DIR__ . '/../../controllers/handler/editHandler/crudHomeHandler.php',
-            '/crudHomePictureHandler'=> __DIR__ . '/../../controllers/handler/editHandler/crudHomePictureHandler.php',
-            '/crud_homes_add'=> __DIR__ . '/../../views/crud/crud_homes_add.php',
-            '/crud_home_picture_add'=> __DIR__ . '/../../views/crud/crud_home_picture_add.php',
-            '/crud_animals' => __DIR__ . '/../../views/crud/crud_animals.php',
-            '/crud_animals_update' => __DIR__ . '/../../views/crud/crud_animals_update.php',
-            '/crudAnimalHandler'=> __DIR__ . '/../../controllers/handler/editHandler/crudAnimalHandler.php',
-            '/crud_animals_add'=> __DIR__ . '/../../views/crud/crud_animals_add.php',
-            '/crud_animal_picture_add'=> __DIR__ . '/../../views/crud/crud_animal_picture_add.php',
-            '/crudAnimalPictureHandler'=> __DIR__ . '/../../controllers/handler/editHandler/crudAnimalPictureHandler.php',
-            '/crud_services' => __DIR__ . '/../../views/crud/crud_services.php',
-            '/crud_services_update' => __DIR__ . '/../../views/crud/crud_services_update.php',
-            '/crudServiceHandler'=> __DIR__ . '/../../controllers/handler/editHandler/crudServiceHandler.php',
-            '/crud_services_add'=> __DIR__ . '/../../views/crud/crud_services_add.php',
-            '/crud_opening_hours'=> __DIR__ . '/../../views/crud/crud_opening_hours.php',
-            '/crud_opening_hours_update'=> __DIR__ . '/../../views/crud/crud_opening_hours_update.php',
-            '/crudOpeningHoursHandler'=> __DIR__ . '/../../controllers/handler/editHandler/crudOpeningHoursHandler.php',
-            '/register' => __DIR__ . '/../../views/register.view.php',
-            '/registerHandler'=> __DIR__ . '/../../controllers/handler/RegisterHandler.php',
-            '/animal_details' => __DIR__ . '/../../views/animal_details.php',
-            '/viewsHandler' => __DIR__ . '/../../controllers/handler/viewsHandler.php',
-            '/animal_views' => __DIR__ . '/../../views/animal_views.php',
-            '/resetViewsHandler' => __DIR__ . '/../../controllers/handler/resetViewsHandler.php',
-            '/security_issuer' => __DIR__ . '/../../controllers/handler/security_issuer.php',
-            
+            '/'  => 'app/views/index.view.php',
+            '/services' => 'app/views/services.view.php',
+            '/homes' => 'app/views/homes.view.php',
+            '/connection' => 'app/views/connection.view.php',
+            '/contact' => 'app/views/contact.view.php',
+            '/admin' => 'app/views/admin_area.view.php',
+            '/vet' => 'app/views/vet_area.view.php',
+            '/employee' => 'app/views/employee_area.view.php',
+            '/information'=> 'app/views/information.php',
+            '/opinionStatusUpdate' => 'app/controllers/handler/opinionStatusUpdate.php',
+            '/employee_validation_opinion' => 'app/views/employee_validation_opinions.php',
+            '/opinionSend' => 'app/controllers/handler/opinionSend.php',
+            '/admin_area'=> 'app/views/admin_area.view.php',
+            '/employee_area'=> 'app/views/employee_area.view.php',
+            '/vet_area'=> 'app/views/vet_area.view.php',
+            '/homeDetails'=> 'app/views/homeDetails.php',
+            '/homeDetailsHandler'=> 'app/controllers/handler/homeDetailsHandler.php',
+            '/employee_food_form' => 'app/views/employee_food_form.php',
+            '/employee_report_handler' => 'app/controllers/handler/employee_report_handler.php',
+            '/vet_visit_form' => 'app/views/vet_visit_form.php',
+            '/vet_reports_for_admin' => 'app/views/vet_reports_for_admin.php',
+            '/end_session' =>'app/controllers/handler/endSession.php',
+            '/crud_homes' =>'app/views/crud/crud_homes.php',
+            '/crud_homes_update' =>'app/views/crud/crud_homes_update.php',
+            '/crudHomeHandler'=> 'app/controllers/handler/editHandler/crudHomeHandler.php',
+            '/crudHomePictureHandler'=> 'app/controllers/handler/editHandler/crudHomePictureHandler.php',
+            '/crud_homes_add'=> 'app/views/crud/crud_homes_add.php',
+            '/crud_home_picture_add'=> 'app/views/crud/crud_home_picture_add.php',
+            '/crud_animals' =>'app/views/crud/crud_animals.php',
+            '/crud_animals_update' =>'app/views/crud/crud_animals_update.php',
+            '/crudAnimalHandler'=> 'app/controllers/handler/editHandler/crudAnimalHandler.php',
+            '/crud_animals_add'=> 'app/views/crud/crud_animals_add.php',
+            '/crud_animal_picture_add'=> 'app/views/crud/crud_animal_picture_add.php',
+            '/crudAnimalPictureHandler'=> 'app/controllers/handler/editHandler/crudAnimalPictureHandler.php',
+            '/crud_services' =>'app/views/crud/crud_services.php',
+            '/crud_services_update' =>'app/views/crud/crud_services_update.php',
+            '/crudServiceHandler'=> 'app/controllers/handler/editHandler/crudServiceHandler.php',
+            '/crud_services_add'=> 'app/views/crud/crud_services_add.php',
+            '/crud_opening_hours'=> 'app/views/crud/crud_opening_hours.php',
+            '/crud_opening_hours_update'=> 'app/views/crud/crud_opening_hours_update.php',
+            '/crudOpeningHoursHandler'=> 'app/controllers/handler/editHandler/crudOpeningHoursHandler.php',
+            '/register' =>'app/views/register.view.php',
+            '/registerHandler'=> 'app/controllers/handler/RegisterHandler.php',
+            '/animal_details' => 'app/views/animal_details.php',
+            '/viewsHandler' => 'app/controllers/handler/viewsHandler.php',
+            '/animal_views' => 'app/views/animal_views.php',
+            '/resetViewsHandler' => 'app/controllers/handler/resetViewsHandler.php',
+            '/security_issuer' => 'app/controllers/handler/security_issuer.php',
         ];
-        var_dump($routes);
+        
         if (array_key_exists($uri, $routes)) {
             require $routes[$uri];
             return true;
         } else {
             http_response_code(404);
-            require __DIR__ . '/../../views/404.php';
+            require 'app/views/404.php';
             echo "La page demandée n'existe pas.";
             die();
         }
-    }
-}
 
-// Exemple d'utilisation du routeur
-$navbar = new Navbar();
-$navbar->router($navbar->getCurrentUri());
+    }
+
+}

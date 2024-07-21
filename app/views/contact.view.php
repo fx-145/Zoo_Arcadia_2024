712d222f1fbb3e9171e7f8c2f64fb46038442979
fx-145 <fxlecoeur@gmail.com>
2024-07-21 07:54:24 +0200

    reset test mail

--- app/views/contact.view.php
+++ app/views/contact.view.php
@@ -20,7 +20,7 @@ session_start(); ?>
                 <h2 class="text-center mb-4">Contactez-nous</h2>
 
                 <!-- Formulaire de contact <form action="app/controllers/handler/MailHandler.php" method="post">-->
-                <form action="MailHandler" method="post">
+                <form action="app/controllers/handler/MailHandler.php" method="post">
                     <!-- Génération du token CSRF -->
                     <?php include "app/controllers/handler/security_issuer.php"; ?>
                     
